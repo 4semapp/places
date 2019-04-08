5a634976a33225f2a8c7f3d2e565e55fa090f908
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-04-08 10:43:32 +0200

    working navigation with fragments

--- app/src/main/java/com/tvestergaard/places/MainActivity.kt
+++ app/src/main/java/com/tvestergaard/places/MainActivity.kt
@@ -1,34 +1,47 @@
 package com.tvestergaard.places
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import java.lang.RuntimeException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    AppCompatActivity(),
+    CameraFragment.Parent,
+    ContributeFragment.Parent,
+    HomeFragment.Parent,
+    SearchFragment.Parent {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+    private val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        val fragment: Fragment = when (item.itemId) {
+            R.id.navigation_home -> HomeFragment.newInstance()
+            R.id.navigation_search -> SearchFragment.newInstance()
+            R.id.navigation_contribute -> ContributeFragment.newInstance()
+            R.id.navigation_camera -> CameraFragment.newInstance()
+            else -> throw RuntimeException("unhandled fragment type ${item.itemId}.")
         }
-        false
+
+        show(fragment)
+        true
+    }
+
+    private fun show(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.fragmentContainer, fragment)
+            .commitAllowingStateLoss();
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(navigationListener)
+        show(HomeFragment())
     }
 }
---
+++ app/src/main/java/com/tvestergaard/places/SearchFragment.kt
@@ -0,0 +1,42 @@
+package com.tvestergaard.places
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SearchFragment : Fragment() {
+
+    private var parent: Parent? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Parent) {
+            parent = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        parent = null
+    }
+
+    interface Parent {
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            SearchFragment().apply {
+                arguments = Bundle()
+            }
+    }
+}
